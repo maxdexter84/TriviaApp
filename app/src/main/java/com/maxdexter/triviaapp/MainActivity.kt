ff5bb342f0e8dbe741a7d8748b063577b7eea506
maxdexter <maxdexter@bk.ru>
2020-08-19 16:38:53 +0300

    дабавил navigation.xml и настроил NavHostController

--- app/src/main/java/com/maxdexter/triviaapp/MainActivity.kt
+++ app/src/main/java/com/maxdexter/triviaapp/MainActivity.kt
@@ -11,9 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        binding.hello.text = "Fuck"
+
 
     }
 }
