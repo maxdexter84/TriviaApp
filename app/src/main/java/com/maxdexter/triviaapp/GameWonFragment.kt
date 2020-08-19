c65368ba957c280b28ca08d05cc640fae568bd50
maxdexter <maxdexter@bk.ru>
2020-08-19 14:37:08 +0300

    ДОбавил GameWonFragment.kt

---
+++ app/src/main/java/com/maxdexter/triviaapp/GameWonFragment.kt
@@ -0,0 +1,23 @@
+package com.maxdexter.triviaapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.maxdexter.triviaapp.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentGameWonBinding>(inflater, R.layout.fragment_game_won, container, false)
+        return binding.root
+    }
+
+}
